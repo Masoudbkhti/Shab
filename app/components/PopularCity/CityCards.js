6fbafa3c70ccb32b715780d6f3b9c35d7319b556
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-07-31 22:03:04 +0330

    Add: (cities-branch) add CityCards.js

---
+++ app/components/PopularCity/CityCards.js
@@ -0,0 +1,40 @@
+import Typography from '@mui/material/Typography';
+import Grid from "@mui/material/Grid";
+import styles from "./Image.module.css"
+
+const CityCards = ({tour}) => {
+    return (
+        <>
+            <Grid item xs={2}>
+                <article className={styles.article}>
+                    <img
+                        src={tour.image}
+                        alt="image"
+                        className={styles.img}
+                    />
+                    <h1 className={styles.headerR}>
+                        <Typography
+                            variant="body2"
+                            component="p"
+                            fontWeight="bold"
+                        >
+                            {tour.name}
+                        </Typography>
+                    </h1>
+                    <h1 className={styles.headerL}>
+                        <Typography
+                            variant="body2"
+                            component="p"
+                            fontWeight="bold"
+                            marginRight={9}
+                        >
+                            {tour.id} اقامتگاه
+                        </Typography>
+                    </h1>
+                </article>
+            </Grid>
+        </>
+    )
+};
+
+export default CityCards;
