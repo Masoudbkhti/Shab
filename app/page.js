ea717d635a66fe64e051dc443a0276bf51baa48c
Mahdi Forootan <forotan1376@gmail.com>
2023-07-31 21:20:56 +0330

    the UnderTopBanner Component changed and the code move to the bottom of TopBanner component.

--- app/page.js
+++ app/page.js
@@ -8,7 +8,6 @@ export default function Home() {
     <>
       <Header />
       <TopBanner />
-      <UnderTopBanner/>
       <Typography variant="body" component="h2">
         سلام
       </Typography>
