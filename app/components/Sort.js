e8ffd711b46d84b2ec47eee9b1e14d312d74ccc8
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-07-31 13:18:30 +0330

    styling Sort Component

--- app/components/Sort.js
+++ app/components/Sort.js
@@ -1,19 +1,35 @@
-import { Box, Typography } from "@mui/material";
+import { Button, Link, Box, Typography } from "@mui/material";
 export default function Sort() {
+  const buttonStyle = {
+    color: "black",
+    borderRadius: "50px",
+    paddingY: "10px",
+    paddingX: "20px",
+    "&:hover": { backgroundColor: "#F8F8FE" },
+  };
   return (
     <Box
       sx={{
-        width: "50%",
+        width: "40%",
         display: "flex",
+        alignItems: "center",
         justifyContent: "space-between",
         marginY: "50px",
       }}
     >
-      <Typography>مرتب سازی</Typography>
-      <Typography>محبوب‌ترین</Typography>
-      <Typography>ارزان‌ترین</Typography>
-      <Typography>گران‌ترین</Typography>
-      <Typography>بالاترین امتیاز</Typography>
+      <Typography>مرتب سازی:</Typography>
+      <Button sx={buttonStyle} href="/" component={Link}>
+        محبوب‌ترین
+      </Button>
+      <Button sx={buttonStyle} href="/" component={Link}>
+        ارزان‌ترین
+      </Button>
+      <Button sx={buttonStyle} href="/" component={Link}>
+        گران‌ترین
+      </Button>
+      <Button sx={buttonStyle} href="/" component={Link}>
+        بالاترین امتیاز
+      </Button>
     </Box>
   );
 }
