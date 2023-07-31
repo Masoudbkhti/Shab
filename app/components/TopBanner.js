9e42e7eedf4bfc53a63cb579198a3d241bd0bc68
Mahdi Forootan <forotan1376@gmail.com>
2023-07-31 15:33:51 +0330

    The pictures added.

--- app/components/TopBanner.js
+++ app/components/TopBanner.js
@@ -6,6 +6,8 @@ export default function TopBanner() {
         sx={{
           display: "flex",
           marginTop: "50px",
+          gap: "10px",
+          bgcolor: "green",
         }}
       >
         <Box
@@ -13,6 +15,7 @@ export default function TopBanner() {
             borderRadius: "10px",
             overflow: "hidden",
             bgcolor: "red",
+            height: "100%",
           }}
         >
           <img
@@ -21,27 +24,51 @@ export default function TopBanner() {
           />
         </Box>
         <Box>
-          <Grid lg={1} sx={{ display: "flex" }}>
+          <Grid lg={1} sx={{ display: "flex", gap: "0px" }}>
             <Box
               sx={{
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "space-between",
-                gap: "5px",
+                gap: "0px",
+                bgcolor: "blue",
+                height: "100%",
               }}
             >
-              <img src="https://s3gw.at.shab.cloud/production/banners/BLwDKLIwX9sQS3KF4AKAmVxmxhvDN1yamDv5QVCj.png" />
-              <img src="https://s3gw.at.shab.cloud/production/banners/BLwDKLIwX9sQS3KF4AKAmVxmxhvDN1yamDv5QVCj.png" />
+              <img
+                src="https://s3gw.at.shab.cloud/production/banners/6wuOKTYwf2EW62MBZoWCC29JUlyju4rHdlbXhoSB.png"
+                alt="اقامت بلند مدت"
+              />
+              <img
+                src="https://s3gw.at.shab.cloud/production/banners/BLwDKLIwX9sQS3KF4AKAmVxmxhvDN1yamDv5QVCj.png"
+                alt="اقامتگاه رزرو آنی"
+              />
             </Box>
             <Box
               sx={{
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "space-between",
+                gap: "0px",
+                bgcolor: "yellow",
               }}
             >
-              <img src="https://s3gw.at.shab.cloud/production/banners/BLwDKLIwX9sQS3KF4AKAmVxmxhvDN1yamDv5QVCj.png" />
-              <img src="https://s3gw.at.shab.cloud/production/banners/BLwDKLIwX9sQS3KF4AKAmVxmxhvDN1yamDv5QVCj.png" />
+              <Box
+              sx={{
+                width:"100%",
+                height:"100%"
+              }}
+              >
+                <img
+                  // src="https://www.shab.ir/images/home/summer.png?w=1024&q=75"
+                  src="https://s3gw.at.shab.cloud/production/banners/drzARrMsa6DCa1lTW5auLKSuErVLqwvXDAdc1wqf.png"
+                  alt="رایگان میزبان شوید"
+                />
+              </Box>
+              <img
+                src="https://s3gw.at.shab.cloud/production/banners/drzARrMsa6DCa1lTW5auLKSuErVLqwvXDAdc1wqf.png"
+                alt="رایگان میزبان شوید"
+              />
             </Box>
           </Grid>
         </Box>
