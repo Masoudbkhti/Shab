5ec2d1b5ece1b68fd44335c7035596a187b2cbb1
alitilkoo <alirezatilkoo@gmail.com>
2023-07-31 12:20:36 +0330

    changing slider

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import { Typography } from "@mui/material";
 import Header from "./components/headerComponent/Header";
 import Navbar from "./components/headerComponent/Navbar";
-export default function Home() {
+import Slider from "./components/Slider";
+import { getLocalData } from "@/json/lib/localdata";
 
+export default async function Home() {
+  const data = await getLocalData();
   return (
     <>
       <Header/>
+      <Slider data={data}/>
       <Typography variant="body" component="h2">
         سلام
       </Typography>
