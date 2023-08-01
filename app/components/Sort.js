65f79d0763773b78a44de3560f3c891ab3d47e42
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-08-02 02:17:11 +0330

    add query to Sort component Urls

--- app/components/Sort.js
+++ app/components/Sort.js
@@ -1,4 +1,5 @@
-import { Button, Link, Box, Typography } from "@mui/material";
+import { Button, Box, Typography } from "@mui/material";
+import Link from "next/link";
 export default function Sort() {
   const buttonStyle = {
     color: "black",
@@ -6,6 +7,7 @@ export default function Sort() {
     paddingY: "10px",
     paddingX: "20px",
     "&:hover": { backgroundColor: "#F8F8FE" },
+    "&:active": { color: "primary" },
   };
   return (
     <Box
@@ -18,18 +20,18 @@ export default function Sort() {
       }}
     >
       <Typography>مرتب سازی:</Typography>
-      <Button sx={buttonStyle} href="/" component={Link}>
-        محبوب‌ترین
-      </Button>
-      <Button sx={buttonStyle} href="/" component={Link}>
-        ارزان‌ترین
-      </Button>
-      <Button sx={buttonStyle} href="/" component={Link}>
-        گران‌ترین
-      </Button>
-      <Button sx={buttonStyle} href="/" component={Link}>
-        بالاترین امتیاز
-      </Button>
+      <Link href={{ query: { sortBy: "5" } }}>
+        <Button sx={buttonStyle}>محبوب‌ترین</Button>
+      </Link>
+      <Link href={{ query: { sortBy: "3" } }}>
+        <Button sx={buttonStyle}>ارزان‌ترین</Button>
+      </Link>
+      <Link href={{ query: { sortBy: "2" } }}>
+        <Button sx={buttonStyle}>گران‌ترین</Button>
+      </Link>
+      <Link href={{ query: { sortBy: "4" } }}>
+        <Button sx={buttonStyle}>بالاترین امتیاز</Button>
+      </Link>
     </Box>
   );
 }
