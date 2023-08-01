42a17e5b3ba54094c6bea46995811de4472a4a48
nasrin jafari <nasrin.jafari770@gmail.com>
2023-08-01 13:44:23 +0430

    design section SiteHistory

--- app/houses/[resId]/City.js
+++ app/houses/[resId]/City.js
@@ -1,13 +1,13 @@
-// "use client"
-import { getLocalData } from '@/json/lib/localdata'
+// // "use client"
+// import { getLocalData } from '@/json/lib/localdata'
 
-export default async function City({param}) {
-    const data = await getLocalData()
-    const filterdata = data.residence.filter((a)=>{
-        return a.id === param
-    })
-    console.log(filterdata);
-  return (
-    sssssssssssssssssssssss
-  )
-}
+// export default async function City({param}) {
+//     const data = await getLocalData()
+//     const filterdata = data.residence.filter((a)=>{
+//         return a.id === param
+//     })
+//     console.log(filterdata);
+//   return (
+//     sssssssssssssssssssssss
+//   )
+// }
--- app/page.js
+++ app/page.js
@@ -2,54 +2,21 @@ import { Typography } from "@mui/material";
 import Header from "./components/headerComponent/Header";
 import TopBanner from "./components/TopBanner";
 import Navbar from "./components/headerComponent/Navbar";
-import TopAccommodations from "./components/TopAccommodations";
 import { getLocalData } from "@/json/lib/localdata";
+import TopResidence from "./components/TopResidence";
 export default async function Home() {
   const data = await getLocalData()
   return (
     <>
-      <Header/>
+      <Header />
       <Typography variant="body" component="h2">
         سلام
       </Typography>
       <Typography variant="body" component="h2">
         سلام
       </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <Typography variant="body" component="h2">
-        سلام
-      </Typography>
-      <TopAccommodations data={data}/>
-      <Navbar/>
+      <TopResidence data={data} />
+      <Navbar />
     </>
   );
 }
