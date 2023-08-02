23a3eaa8a3d8e24477969497a080d91dff0bc394
Mahdi Forootan <forotan1376@gmail.com>
2023-08-02 14:54:15 +0330

    The Rulse component built

---
+++ app/components/houses/Rules.js
@@ -0,0 +1,14 @@
+import { Container, Box, Typography, Divider} from "@mui/material/Container";
+export default function Rules() {
+  return (
+    <Container>
+      <Divider component="div" role="presentation">
+        <Typography variant="h2">قوانین</Typography>
+        <Box >
+        <Box></Box>
+        <Box></Box>
+        </Box>
+      </Divider>
+    </Container>
+  );
+}
