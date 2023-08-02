2ef3d83f8c14eb0a8e2901cf82981aee630ba738
Mahdi Forootan <forotan1376@gmail.com>
2023-08-02 20:15:07 +0330

    The Comment component added

--- app/houses/[resId]/page.js
+++ app/houses/[resId]/page.js
@@ -1,6 +1,7 @@
 import { getLocalData } from "@/json/lib/localdata";
 import Reserve from "../../components/houses/Reserve";
 import Rules from "../../components/houses/Rules";
+import Comments from "@/app/components/houses/Comments";
 
 export default async function Page({ params }) {
   const data = await getLocalData();
@@ -8,8 +9,8 @@ export default async function Page({ params }) {
   return (
     <div>
       <Reserve data={filteredData[0]} />
-      <Rules data={filteredData[0]} />
-      {/* <Comments data={filteredData[0]} /> */}
+      <Rules/>
+      <Comments/>
     </div>
   );
 }
