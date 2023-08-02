45fb537e12a45b56f28527d0d325723e6f9f6074
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-08-02 20:38:10 +0330

    import TopResidence in home page component

--- app/page.js
+++ app/page.js
@@ -2,12 +2,15 @@ import { Typography } from "@mui/material";
 import Header from "./components/headerComponent/Header";
 import TopBanner from "./components/TopBanner";
 import UnderTopBanner from "./components/UnderTopBanner";
+import TopResidence from "./components/TopResidence";
 import Navbar from "./components/headerComponent/Navbar";
 export default function Home() {
 
   return (
     <>
       <Header/>
+      <TopBanner/>
+      <UnderTopBanner/>
       <Typography variant="body" component="h2">
         سلام
       </Typography>
