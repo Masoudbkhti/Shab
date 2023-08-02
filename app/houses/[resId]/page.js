c66623a0dafdaa890afedbc2b87b35b797e1ba94
Mahdi Forootan <forotan1376@gmail.com>
2023-08-02 14:53:07 +0330

    The Rules component added to the page.js page.

--- app/houses/[resId]/page.js
+++ app/houses/[resId]/page.js
@@ -1,5 +1,6 @@
 import { getLocalData } from "@/json/lib/localdata";
 import Reserve from "../../components/houses/Reserve";
+import Rules from "../components/houses/Rules"
 
 export default async function Page({ params }) {
   const data = await getLocalData();
@@ -7,6 +8,7 @@ export default async function Page({ params }) {
   return (
     <div>
       <Reserve data={filteredData[0]} />
+      <Rules data={filteredData[0]}/>
     </div>
   );
 }
