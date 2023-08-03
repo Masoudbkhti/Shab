c9aa0dc112f150834f3f38cabbc537973b5ff3f7
alitilkoo <alirezatilkoo@gmail.com>
2023-08-03 15:12:21 +0330

    using data from database in photolist

--- app/houses/[resId]/page.js
+++ app/houses/[resId]/page.js
@@ -1,5 +1,6 @@
 import { getLocalData } from "@/json/lib/localdata";
 import Reserve from "../../components/houses/Reserve";
+import PhotoList from "@/app/components/PhotoList";
 
 export default async function Page({ params }) {
   const data = await getLocalData();
@@ -7,6 +8,7 @@ export default async function Page({ params }) {
   return (
     <div>
       <Reserve data={filteredData[0]} />
+      <PhotoList data={filteredData[0]} /> 
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Header from "./components/headerComponent/Header";
 import TopBanner from "./components/TopBanner";
 import UnderTopBanner from "./components/UnderTopBanner";
 import Navbar from "./components/headerComponent/Navbar";
+import TopResidence from "./components/TopResidence";
 export default function Home() {
 
   return (
