82ffb98e232df9f0c14ac42f02cb69cfc3139f91
Mahdi Forootan <forotan1376@gmail.com>
2023-08-03 15:34:21 +0330

    The divider tag deleted to decide after, adding margin top to the ghavanin and under div, justify content and align items to the time of start and finished

--- app/components/houses/Rules.js
+++ app/components/houses/Rules.js
@@ -1,26 +1,41 @@
-import { Container, Box, Typography, Divider, Paper } from "@mui/material";
+import {
+  Container,
+  Box,
+  Typography,
+  Grid,
+  Divider,
+  Paper,
+} from "@mui/material";
 import DoneIcon from "@mui/icons-material/Done";
 
-export default function Rules({data}) {
+export default function Rules({ data }) {
   return (
     <Container>
-      <Divider component="div" role="presentation">
-        <Typography variant="h2">قوانین</Typography>
-        <Box sx={{ display: "flex" }}>
-          <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <Typography>
+      <Box>
+        <Typography variant="body1" sx={{ fontWeight: "Bold", marginTop:"20px"}}>
+          قوانین
+        </Typography>
+        {/* <Box sx={{ display: "flex" }}> */}
+        <Grid lg={1} spacing={5} sx={{ display: "flex" }}>
+          <Box sx={{ display: "flex", flexDirection: "column", width: "50%" }}>
+            <Typography sx={{ marginTop:"20px" }}>
               کودکان 2 سال به بالا جزو نفرات محاسبه شده و از آن‌ها هزینه‌ی کامل
               دریافت می‌شود (تا 1 کودک مانعی ندارد).
             </Typography>
             <Typography>نمایش کمتر</Typography>
-            <Box sx={{ display: "flex" }}>
+            <Box sx={{ display: "flex", justifyContent:"space-between" }}>
               <Box
                 sx={{
                   display: "flex",
                   flexDirection: "column",
                   borderRadius: "15px",
-                  border: 1,
+                  border: 2,
                   borderColor: "grey.500",
+                  width:"40%",
+                  height:"70px",
+                  justifyContent:"center",
+                  alignItems:"center"
+
                 }}
               >
                 <Typography>ساعت ورود</Typography>
@@ -31,8 +46,12 @@ export default function Rules({data}) {
                   display: "flex",
                   flexDirection: "column",
                   borderRadius: "15px",
-                  border: 1,
+                  border: 2,
                   borderColor: "grey.500",
+                  width:"40%",
+                  justifyContent:"center",
+                  alignItems:"center"
+                  
                 }}
               >
                 <Typography>ساعت ورود</Typography>
@@ -54,14 +73,19 @@ export default function Rules({data}) {
               </Typography>
             </Box>
           </Box>
+
           <Box sx={{ display: "flex", flexDirection: "column" }}>
-            <Box><Typography>مدارک مورد نیاز</Typography></Box>
+            <Box>
+              <Typography>مدارک مورد نیاز</Typography>
+            </Box>
             <Box></Box>
-            <Box><Typography>شرایط کنسلی و لغو رزرو (سخت گیرانه)</Typography></Box>
+            <Box>
+              <Typography>شرایط کنسلی و لغو رزرو (سخت گیرانه)</Typography>
+            </Box>
             <Box></Box>
           </Box>
-        </Box>
-      </Divider>
+        </Grid>
+      </Box>
     </Container>
   );
 }
