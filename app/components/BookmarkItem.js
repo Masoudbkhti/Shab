624ae2694de165b1605d1c203fd42f3d9a6180eb
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-08-06 13:20:57 +0330

    styling /bookmarks page and adding toEnglishDigits function to utils

--- app/components/BookmarkItem.js
+++ app/components/BookmarkItem.js
@@ -1,17 +1,13 @@
 "use client";
-import { Box, Grid, Paper, Typography } from "@mui/material";
-import Divider from "@mui/material/Divider";
+import { Box, Grid, Paper, Typography, Rating } from "@mui/material";
 import SwiperSlider from "./SwiperSlider";
-import RoomOutlinedIcon from "@mui/icons-material/RoomOutlined";
-import MapsHomeWorkOutlinedIcon from "@mui/icons-material/MapsHomeWorkOutlined";
+import FastUse from "@/app/components/FastUse";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import Rate from "./Rate";
-import ReservationChip from "./ReservationChip";
-import HospitableChip from "./HospitableChip";
 import Link from "next/link";
 import truncateText from "@/utils/truncateText";
+import toEnglishDigits from "@/utils/toEnglishDigits";
 export default function BookmarkItem({
   id,
   img,
@@ -22,7 +18,10 @@ export default function BookmarkItem({
   oldprice,
   fastreserve,
   hospitable,
+  commentNum,
 }) {
+  const rateNum = toEnglishDigits(rate);
+  console.log(rateNum);
   return (
     <Grid item md={6} lg={3}>
       <Paper elevation={1} sx={{ overflow: "hidden", position: "relative" }}>
@@ -37,62 +36,65 @@ export default function BookmarkItem({
             justifyContent: "space-between",
           }}
         >
-          <Link href={`/houses/${id}`}>
-            <Typography sx={{ marginBottom: "10px", fontWeight: "bold" }}>
-              {truncateText(name, 40)}
-            </Typography>
-          </Link>
-          <Box sx={{ display: "flex", gap: "5px" }}>
-            <RoomOutlinedIcon fontSize="medium" sx={{ color: "#969696" }} />
-            <Typography sx={{ fontSize: "12px" }}>{location}</Typography>
-          </Box>
           <Box
             sx={{
               display: "flex",
               alignItems: "center",
-              justifyContent: "space-between",
+              gap: "5px",
             }}
           >
-            <Box sx={{ display: "flex", gap: "5px" }}>
-              <MapsHomeWorkOutlinedIcon
-                fontSize="small"
-                sx={{ color: "#969696" }}
-              />
-            </Box>
-            <Box>
-              <Rate rate={rate} />
-            </Box>
-          </Box>
-          <Box sx={{ display: "flex", gap: "10px", marginY: "10px" }}>
-            {fastreserve && <ReservationChip />}
-            {hospitable && <HospitableChip />}
-          </Box>
+            {fastreserve && <FastUse />}
 
-          <Divider />
+            <Typography sx={{ fontSize: "12px", color: "#969696" }}>
+              {truncateText(location, 40)}
+            </Typography>
+          </Box>
+          <Link href={`/houses/${id}`}>
+            <Typography sx={{ marginBottom: "10px", fontWeight: "bold" }}>
+              {name}
+            </Typography>
+          </Link>
+
           <Box
             sx={{
               display: "flex",
+              flexDirection: "column",
+              gap: "5px",
               justifyContent: "space-between",
               marginTop: "5px",
             }}
           >
-            <Typography color="black" sx={{ fontWeight: "bold" }}>
-              هر شب از
-            </Typography>
-            <Box sx={{ display: "flex", gap: "10px" }}>
-              {oldprice && (
-                <Typography
-                  color="#969696"
-                  sx={{ textDecoration: "line-through" }}
-                >
-                  {oldprice}
-                </Typography>
-              )}
-              <Typography color="black" sx={{ fontWeight: "bold" }}>
-                {price}
+            <Box>
+              <Typography
+                display="inline"
+                sx={{
+                  fontWeight: "bold",
+                  color: "#969696",
+                  textDecoration: "line-through",
+                }}
+              >
+                {oldprice && (
+                  <Typography color="#969696" sx={{ fontWeight: "bold" }}>
+                    هر شب از {oldprice} تومان
+                  </Typography>
+                )}
               </Typography>
-              <Typography color="#969696">تومان</Typography>
             </Box>
+            {oldprice ? (
+              <Box>
+                <Typography>امشب {price} تومان </Typography>
+              </Box>
+            ) : (
+              <Box>
+                <Typography>هر شب {price} تومان </Typography>
+              </Box>
+            )}
+          </Box>
+          <Box sx={{ display: "flex", gap: "5px" }}>
+            <Rating name="read-only" value={toEnglishDigits(rate)} readOnly />
+            <Typography>{rate}</Typography>
+            <Typography>({commentNum} نظر) . </Typography>
+            {hospitable && <Typography>مهمان نواز</Typography>}
           </Box>
         </Box>
       </Paper>
--- app/houses/[resId]/page.js
+++ app/houses/[resId]/page.js
@@ -1,21 +1,22 @@
-import {getLocalData} from "@/json/lib/localdata";
+import { getLocalData } from "@/json/lib/localdata";
 import Reserve from "../../components/houses/Reserve";
 import Rules from "../../components/houses/Rules";
 import Comments from "@/app/components/houses/Comments";
+import { useSelector } from "react-redux";
 
-export default async function Page({params}) {
-    const ShowImage = useSelector((state)=>state.AlbumSlice)
-    console.log(ShowImage)
-    const data = await getLocalData();
-    // const comment = data.comments;
-    const filteredData = data.residence.filter((res) => res.id == params.resId);
-    return (
-        <>
-            <HouseShow data={filteredData[0]}/>
-            <Reserve data={filteredData[0]}/>
-            <Rules/>
-            <CommentRate/>
-            <SingleComment/>
-        </>
-    );
+export default async function Page({ params }) {
+  const ShowImage = useSelector((state) => state.AlbumSlice);
+  console.log(ShowImage);
+  const data = await getLocalData();
+  // const comment = data.comments;
+  const filteredData = data.residence.filter((res) => res.id == params.resId);
+  return (
+    <>
+      <HouseShow data={filteredData[0]} />
+      <Reserve data={filteredData[0]} />
+      <Rules />
+      <CommentRate />
+      <SingleComment />
+    </>
+  );
 }
