ad48c721f09011022e16b4a4055cfa12a650071e
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-08-06 12:25:58 +0330

    fix: (master-branch:MF) fix some change in page.js(house) and Slider.js

--- app/components/Slider.js
+++ app/components/Slider.js
@@ -1,8 +1,8 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, {useRef, useState} from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import {Swiper, SwiperSlide} from "swiper/react";
 import ResCardHome from "./ResCardHome";
 import Grid from "@mui/material/Grid";
 
@@ -17,43 +17,40 @@ import "swiper/css/navigation";
 import "./slider.css";
 
 // import required modules
-import { FreeMode, Navigation } from "swiper/modules";
-import { Container } from "@mui/material";
+import {FreeMode, Navigation} from "swiper/modules";
+import {Container} from "@mui/material";
 
 export default function Slider({data}) {
 //  console.log(data.cities)
 // console.log(data.residence)
-  return (
-    <Container>
-      <Grid container justifyContent="center" spacing={2} mt={4}>
-        <Swiper
-          slidesPerView={3.5}
-          spaceBetween={30}
-          freeMode={true}
-          navigation={true}
-          modules={[FreeMode, Navigation]}
-          className="mySwiper"
-        >
-          {data.residence.map((res) => (
-                  <SwiperSlide ><ResCardHome 
-                  img={res.image.cover}
-                  name={res.title}
-                  location={res.location}
-                  type={res.type}
-                  person={res.person}
-                  room={res.room}
-                  rate={res.rate}
-                  price={res.price}
-                  fastreserve={res.fastreserve}
-                />
-                
-                 </SwiperSlide>
-            
-          ))}
-
-            
-        </Swiper>
-      </Grid>
-    </Container>
-  );
+    return (
+        <Container>
+            <Grid container justifyContent="center" spacing={2} mt={4}>
+                <Swiper
+                    slidesPerView={3.5}
+                    spaceBetween={30}
+                    freeMode={true}
+                    navigation={true}
+                    modules={[FreeMode, Navigation]}
+                    className="mySwiper"
+                >
+                            {data && data.residence && data.residence.map((res) => (
+                                <SwiperSlide key={res.id}> {/* Assuming 'id' is a unique identifier */}
+                                    <ResCardHome
+                                        img={res.image["cover"]}
+                                        name={res.title}
+                                        location={res.location}
+                                        type={res.type}
+                                        person={res.person}
+                                        room={res.room}
+                                        rate={res.rate}
+                                        price={res.price}
+                                        fastreserve={res.fastreserve}
+                                    />
+                                </SwiperSlide>
+                            ))}
+                </Swiper>
+            </Grid>
+        </Container>
+    );
 }
--- app/houses/[resId]/page.js
+++ app/houses/[resId]/page.js
@@ -1,11 +1,14 @@
 import {getLocalData} from "@/json/lib/localdata";
 import Reserve from "../../components/houses/Reserve";
 import Rules from "../../components/houses/Rules";
-import Comments from "@/app/components/houses/Comments";
+// import Comments from "@/app/components/houses/Comments";
+import HouseShow from "@/app/components/HouseShow/HouseShow";
+import CommentRate from "@/app/components/houses/CommentRate";
+import SingleComment from "@/app/components/houses/SingleComment";
 
 export default async function Page({params}) {
-    const ShowImage = useSelector((state)=>state.AlbumSlice)
-    console.log(ShowImage)
+    // const ShowImage = useSelector((state)=>state.AlbumSlice)
+    // console.log(ShowImage)
     const data = await getLocalData();
     // const comment = data.comments;
     const filteredData = data.residence.filter((res) => res.id == params.resId);
