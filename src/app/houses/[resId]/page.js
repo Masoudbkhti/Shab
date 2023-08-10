5b855e049488d46b5590f77af8e237172b74aca3
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-08-10 15:42:11 +0330

    add: (master-branch:MF) undo change

--- src/app/houses/[resId]/page.js
+++ src/app/houses/[resId]/page.js
@@ -10,7 +10,7 @@ import styles from "./../../../components/HousesComponents/HouseShow/house.modul
 import ImageListModal from "@/src/components/HousesComponents/ImageList/ImageListModal";
 export default async function Page({ params }) {
   const data = await getLocalData();
-  const filteredData = data.residence.filter((res) => res.id === params.resId);
+  const filteredData = data.residence.filter((res) => res.id == params.resId);
   return (
     <>
       <Box sx={{ marginTop: "80px", backgroundColor: "#fff" }}>
