ef0097e583eaeac52e103182ee8e613fceec77da
Mahdi Forootan <forotan1376@gmail.com>
2023-08-11 19:08:56 +0330

    The width 100% and width 250px added.

--- src/components/HomeComponents/topBanner/UnderTopBanner.js
+++ src/components/HomeComponents/topBanner/UnderTopBanner.js
@@ -1,31 +1,37 @@
-import { Container, Box, Paper, Typography } from "@mui/material";
+import {  Box, Typography } from "@mui/material";
 export default function UnderTopBanner() {
-  const iconStyle = { marginTop: "5px", fontWeight: "bold", textAlign:"center", width:"180px" };
+  const iconStyle = {
+    marginTop: "5px",
+    marginBottom:"10px",
+    fontWeight: "bold",
+    textAlign: "center",
+    width: "250px",
+  };
   return (
-    <Paper
+    <Box
       elevation={1}
       sx={{
         display: "flex",
-        gap: "140px",
-        margin: "20px 0 0 0",
-        borderRadius: "8px",
-        padding: "10px",
-        overflowX: "scroll",
+        // justifyContent:"space-evenly",
+        // gap: { md: "10", lg: "15px" },
+        width: "100%",
+        // margin: "30px auto",
+        padding: "15px",
+        overflowX: { xs: "scroll", lg: "visible" },
         "&::-webkit-scrollbar": { display: "none" },
       }}
     >
       <Box
         sx={{
           display: "flex",
-          gap:"140px",
-          margin:"20px 0 0 0",
-          borderRadius: "8px",
-          padding: "10px",
-          overflowX:"scroll",
-          '&::-webkit-scrollbar': {display: "none"}
-                  }}
+          flexDirection: "column",
+          alignItems: "center",
+          // marginTop: "20px",
+          width: "300px",
+          // justifyContent: "space-between",
+        }}
       >
-        <Container
+        <Box
           sx={{
             width: "90px",
             height: "50px",
@@ -34,8 +40,8 @@ export default function UnderTopBanner() {
           }}
         >
           <img src="https://www.shab.ir/images/product/insurance.svg?w=96&q=75" />
-        </Container>
-        <Typography sx={iconStyle} variant="h6" component="h6">
+        </Box>
+        <Typography sx={iconStyle} variant="body1">
           بیمه رایگان اقامت
         </Typography>
       </Box>
@@ -44,10 +50,12 @@ export default function UnderTopBanner() {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          // justifyContent: "space-between",
+          // marginTop: "20px",
           width: "300px",
         }}
       >
-        <Container
+        <Box
           sx={{
             width: "90px",
             height: "50px",
@@ -56,8 +64,8 @@ export default function UnderTopBanner() {
           }}
         >
           <img src="https://www.shab.ir/images/product/discussion.svg?w=48&q=75" />
-        </Container>
-        <Typography sx={iconStyle} variant="h6" component="h6">
+        </Box>
+        <Typography sx={iconStyle} variant="body1">
           گفت‌وگو با میزبان پیش از پرداخت
         </Typography>
       </Box>
@@ -66,10 +74,12 @@ export default function UnderTopBanner() {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          // justifyContent: "space-between",
+          // marginTop: "20px",
           width: "300px",
         }}
       >
-        <Container
+        <Box
           sx={{
             width: "90px",
             height: "50px",
@@ -78,8 +88,8 @@ export default function UnderTopBanner() {
           }}
         >
           <img src="https://www.shab.ir/images/product/clean-house.svg?w=48&q=75" />
-        </Container>
-        <Typography sx={iconStyle} variant="h6" component="h6">
+        </Box>
+        <Typography sx={iconStyle} variant="body1">
           تضمین نظافت اقامتگاه
         </Typography>
       </Box>
@@ -88,6 +98,8 @@ export default function UnderTopBanner() {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          // justifyContent: "space-between",
+          // marginTop: "20px",
           width: "300px",
         }}
       >
@@ -104,10 +116,10 @@ export default function UnderTopBanner() {
             sx={{ width: "100%", height: "100%" }}
           />
         </Box>
-        <Typography sx={iconStyle} variant="h6" component="h6">
+        <Typography sx={iconStyle} variant="body1">
           پشتیبانی تا پایان سفر
         </Typography>
       </Box>
-    </Paper>
+    </Box>
   );
 }
