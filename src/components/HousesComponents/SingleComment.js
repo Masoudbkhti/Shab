225c26391afdedc049446a7a0bd7fd77e0b4684a
Mahdi Forootan <forotan1376@gmail.com>
2023-08-11 21:47:23 +0330

    The useless code is deleted

--- src/components/HousesComponents/SingleComment.js
+++ src/components/HousesComponents/SingleComment.js
@@ -1,14 +1,8 @@
 // 'use client';
 import { getLocalData } from "@/json/lib/localdata";
-import { Box, Typography, Grid } from "@mui/material";
-import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
-import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
-import UnderTopBanner from "../HomeComponents/topBanner/UnderTopBanner";
-import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import { Box } from "@mui/material";
 import SingleCommentSwiped from "./SingleCommentSwiped";
-// import Link from "next/link";
-// import Popup from 'reactjs-popup';
-// import 'reactjs-popup/dist/index.css';
+
 
 export default async function SingleComment({ data }) {
   const commentdata = await getLocalData();
