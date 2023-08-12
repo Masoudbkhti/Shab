0d38ab581c0f5d7ecab12494bb0e54e67875d160
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-08-12 14:58:35 +0330

    add Skeleton Loading with setTimeOut for Card component

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -3,8 +3,10 @@ import Sort from "@/src/components/SearchComponents/Sort";
 import Menu from "@/src/components/GlobalComponents/headerComponents/Menu";
 import SortedHouses from "@/src/components/SearchComponents/SortedHouses";
 import { getLocalData } from "@/json/lib/localdata";
+
 export default async function Search() {
   const data = await getLocalData();
+
   return (
     <>
       <Container
